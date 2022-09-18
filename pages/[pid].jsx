4281c4dc4f83a1e236c5bfbbf7a72217d8d7190e
boonpat.papob <boonpat.papob@gmail.com>
2022-09-18 16:21:15 +0700

    Pre-Render: getStaticPaths

---
+++ pages/[pid].jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import fs from "fs/promises";
+import path from "path";
+const ProductDetailPage = (props) => {
+  const { loadedProduct } = props;
+
+  // if (!loadedProduct) {
+  //   return <p>Loading....</p>;
+  // }
+
+  return (
+    <>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </>
+  );
+};
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+  console.log("Regenerating .....");
+  const filePath = path.join(process.cwd(), "data/dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { pid: "p1" } }],
+    fallback: "blocking",
+  };
+}
+
+export default ProductDetailPage;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,47 @@
 import React from "react";
 import fs from "fs/promises";
 import path from "path";
+import Link from "next/link";
 const HomePage = (props) => {
   const { products } = props;
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
 };
 
 export async function getStaticProps() {
+  console.log("Regenerating .....");
   const filePath = path.join(process.cwd(), "data/dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
