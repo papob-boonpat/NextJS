20efe55419ba50691d444f0346bd6d92872112f9
boonpat.papob <boonpat.papob@gmail.com>
2022-09-18 19:02:58 +0700

    Pre-Render: Prefetch with getStaticProps

--- pages/last-sale.jsx
+++ pages/last-sale.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const LastSalePage = () => {
-  const [sales, setSales] = useState();
+const LastSalePage = (props) => {
+  const [sales, setSales] = useState(props.sales);
 
   const { data, error } = useSWR(
     "https://dummy-f3781-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json",
@@ -25,7 +25,7 @@ const LastSalePage = () => {
     return <p>Fail to Load</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading.....</p>;
   }
 
@@ -39,5 +39,23 @@ const LastSalePage = () => {
     </ul>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://dummy-f3781-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return { props: { sales: transformedSales }, revalidate: 10 };
+}
 
 export default LastSalePage;
